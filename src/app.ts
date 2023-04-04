c27cf4998a98339f777fe32210a2ff86e3902356
Dang Tinh <>
2023-04-04 09:52:23 +0700

    docker

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
-import express, {Express} from 'express'
+import express, {Express, Request, Response} from 'express'
 import * as http from "http";
 import {Server} from "net";
 import SocketController from './controller/socket.controller'
 import databaseConfig from './config/database'
 let mongoose = require('mongoose')
+const Youtube = require('youtube-stream-url');
 class App{
     public server:Server
     public readonly app:Express
@@ -13,7 +14,7 @@ class App{
         this.server = http.createServer(this.app)
         new SocketController(this.server)
         App.connectMongo()
-
+        this.route()
     }
 
     private static connectMongo(){
@@ -27,5 +28,21 @@ class App{
             console.log(e)
         }
     }
+
+    route()
+    {
+        this.app.get('/',async function (req:Request,res:Response){
+            let videoId = req.query['video-id']
+            console.log(videoId)
+            try{
+                let detail = await Youtube.getInfo({url: 'https://www.youtube.com/watch?v='+videoId});
+                return res.json(detail)
+            }catch (e:any){
+                return res.json({})
+            }
+
+        })
+    }
+
 }
 export default App
