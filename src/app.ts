4fdfcf12d0dac225eae38596e5d3ddb9b5f48e20
Dang Tinh <>
2023-04-18 23:35:11 +0700

    docker

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,10 @@ import * as http from "http";
 import {Server} from "net";
 import SocketController from './controller/socket.controller'
 import databaseConfig from './config/database'
+import {HttpsProxyAgent} from "https-proxy-agent";
 let mongoose = require('mongoose')
 const Youtube = require('youtube-stream-url');
+const axios = require('axios')
 class App{
     public server:Server
     public readonly app:Express
@@ -46,19 +48,42 @@ class App{
         })
 
         this.app.get('/vtv',(req:Request,res:Response)=>{
+
             var fetchUrl = require("fetch").fetchUrl;
             let pattern = /<script id="__NEXT_DATA__" type="application\/json">(.*?)<\//;
-            fetchUrl(req.query['url'],function (error:any,meta:any,body:any){
-                if(error!==null){
-
-                    return res.json({})
-                }
-                let response = body.toString()
-                let result = response.match(pattern);
-                return res.json({
-                    data:result[0].toString()
-                })
+            // const httpsAgent = new HttpsProxyAgent('http://113.161.131.43:80');
+            const httpsAgent = new HttpsProxyAgent('http://45.124.95.153:3128');
+            console.log(req.query['url'])
+            axios.create(httpsAgent).get(req.query['url'] as string,{
+                proxy:false,
+                httpsAgent:httpsAgent
             })
+                .then((response: any) => {
+                    // console.log(ressponse.data)
+                    let result = response.data.match(pattern);
+                    return res.json({
+                        data:result[0].toString()
+                    })
+                }).catch((err: any) => {
+                console.log(err.toString())
+                return res.json({})
+            })
+
+
+
+            // fetchUrl(req.query['url'],{
+            //     agent:httpsAgent
+            // },function (error:any,meta:any,body:any){
+            //     if(error!==null){
+            //         console.log(error)
+            //         return res.json({data:''})
+            //     }
+            //     let response = body.toString()
+            //     let result = response.match(pattern);
+            //     return res.json({
+            //         data:result[0].toString()
+            //     })
+            // })
         })
     }
 
